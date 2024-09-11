7acf52606d23d8d8c2be10bf3a32f4ecd4eb7535
powerexploit <dobhal.ankit@protonmail.com>
2024-09-12 01:15:30 +0530

    fixed the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="sniperpy",
-    version="1.0.0",
+    version="1.0.1",
     author="Ankit Dobhal",
     author_email="dobhal.ankit@protonmail.com",
     description="A Python package to analyze SMB2 packets from pcap files",
